b1c93c8631cf3c7e66fa4c554bcc7bdc9e9b41cf
tondrika <antoniocarlos.ti@hotmail.com.br>
2018-08-21 20:27:38 -0300

    Commit Inicial - Frontend Angular / Backend Spring 2.0.4

---
+++ transport-api/src/main/java/br/com/udx/transportapi/TransportApiApplication.java
@@ -0,0 +1,12 @@
+package br.com.udx.transportapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransportApiApplication.class, args);
+	}
+}
